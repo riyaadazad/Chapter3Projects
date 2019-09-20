46913c7f261b927e981b5c8b696e84a6dde41708
Riyaad Azad <azad.riyaad@gmail.com>
2019-09-20 00:57:42 +0000

    Update

--- src/CompareStrings.java
+++ src/CompareStrings.java
@@ -1,8 +1,13 @@
 public class CompareStrings {
     /*
-     *
+     * Write a program that reads in two strings, compares them, and prints out the one that comes first alphabetically (the "smaller" of the the two).
+     * You will need to use the compareTo method because you are comparing two of the same type of objects.
+     * LEARNING POINT: Try upper versus lower case, and see what happens - don't worry about the results though - just notice what they are.)
+     * Note the double quote marks in the answers below - they should appear.
      */
     public static void main(String[] args) {
+        // TODO Read in two strings
 
+        // TODO Compare strings with compareTo method and print out the string that comes first alphabetically
     }
 }
