1a3239c4864ef6866f44b20a14ca19e529190219
Riyaad Azad <azad.riyaad@gmail.com>
2019-09-20 00:30:29 +0000

    Update

--- src/CompareInt.java
+++ src/CompareInt.java
@@ -1,5 +1,6 @@
 public class CompareInt {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // TODO Prompt the user to enter 2 ints and then read them in
+        // TODO Compare the ints with inequalities and tell which is smaller or if they are the same
     }
 }
