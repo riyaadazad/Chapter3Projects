361fe3bd8f81dd604504c7ed7fc3dea339cd2116
Riyaad Azad <azad.riyaad@gmail.com>
2019-10-04 14:12:53 +0000

    Update

--- src/Asterisks.java
+++ src/Asterisks.java
@@ -23,9 +23,36 @@ public class Asterisks {
      */
 
     public static void main(String[] args) {
-        // TODO Part A
-        for (int i = 1; i <= 10; i++) {
-
+        // Part A
+        for (int i = 1; i <= 1; i++) {
+            System.out.println("*");
+            for (i = 1; i <= 1; i++) {
+                System.out.println("**");
+            }
+            for (i = 1; i <= 1; i++) {
+                System.out.println("***");
+            }
+            for (i = 1; i <= 1; i++) {
+                System.out.println("****");
+            }
+            for (i = 1; i <= 1; i++) {
+                System.out.println("*****");
+            }
+            for (i = 1; i <= 1; i++) {
+                System.out.println("******");
+            }
+            for (i = 1; i <= 1; i++) {
+                System.out.println("*******");
+            }
+            for (i = 1; i <= 1; i++) {
+                System.out.println("********");
+            }
+            for (i = 1; i <= 1; i++) {
+                System.out.println("*********");
+            }
+            for (i = 1; i <= 1; i++) {
+                System.out.println("**********");
+            }
         }
 
         // TODO Part B
