9bb8f34a661da66c6272abbde78c730b725549bc
Riyaad Azad <azad.riyaad@gmail.com>
2019-10-04 14:33:33 +0000

    Update

--- src/Asterisks.java
+++ src/Asterisks.java
@@ -34,9 +34,9 @@ public class Asterisks {
 
         // TODO Part B
         System.out.println("Part B:");
-        for (int i = 10; i >= 1; i--) {
-            for (int j = 10; j >= i; j--) {
-                System.out.print("**********");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
