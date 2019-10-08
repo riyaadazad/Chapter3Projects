d696e9cb0024b09929cf52a8d4dd85c94391e0ef
Riyaad Azad <azad.riyaad@gmail.com>
2019-10-08 14:25:42 +0000

    Update

--- src/Asterisks.java
+++ src/Asterisks.java
@@ -74,9 +74,6 @@ public class Asterisks {
         for (int i = 1; i <= 10; i++) {
             for (int j = 10; j >= i; j--) {
                 System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
                 for (int k = 1; k <= i; k++) {
                     System.out.print("*");
                 }
