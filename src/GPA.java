272a5e67a351f3bb4d7305970e80e718d290975b
Riyaad Azad <azad.riyaad@gmail.com>
2019-10-10 15:13:58 +0000

    Update

--- src/GPA.java
+++ src/GPA.java
@@ -23,60 +23,87 @@ public class GPA {
     /**
      * Calculates the GPA of one grade into its
      * numerical equivalent
+     *
      * @param grade the grade
      * @return the GPA
      */
     private static double getGPA(String grade) {
-        // TODO Calculate the student's Grade Point Average (GPA) and convert it to a numerical values
+        // Calculate the student's Grade Point Average (GPA) and convert it to a numerical values
+        double GPA = 0;
+        return GPA;
     }
 
     public static void main(String[] args) {
-        // TODO Use a loop to enter grades until sentinel "Q"
+        // Use a loop to enter grades until sentinel "Q"
         String userInput = "";
+        double GPA = 0.0;
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a grade or Q to quit: ");
+        while (userInput != SENTINEL) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter a grade or Q to quit: ");
+            userInput = input.next();
 
-        if (userInput == SENTINEL) {
-            System.exit(1);
-        } else {userInput = input.next();}
-
-        // TODO Use a switch statement to determine the GPA
-        switch (userInput) {
-            case "A+" :
-                break;
-            case "A" :
-                break;
-            case "A-" :
-                break;
-            case "B+" :
-                break;
-            case "B" :
-                break;
-            case "B-" :
-                break;
-            case "C+" :
-                break;
-            case "C" :
-                break;
-            case "C-" :
-                break;
-            case "D+" :
-                break;
-            case "D" :
-                break;
-            case "D-" :
-                break;
-            case "F+" :
-                break;
-            case "F" :
-                break;
-            case "F-" :
-                break;
-            default : // Print "Invalid Grade" for an invalid grade
-                System.out.println("Invalid grade.");
-                break;
-
+            // Use a switch statement to determine the GPA
+            switch (userInput) {
+                case "A+":
+                    GPA = 4.3;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "A":
+                    GPA = 4.0;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "A-":
+                    GPA = 3.7;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "B+":
+                    GPA = 3.3;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "B":
+                    GPA = 3.0;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "B-":
+                    GPA = 2.7;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "C+":
+                    GPA = 2.3;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "C":
+                    GPA = 2.0;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "C-":
+                    GPA = 1.7;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "D+":
+                    GPA = 1.3;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "D":
+                    GPA = 1.0;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "D-":
+                    GPA = 0.7;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case "F":
+                    GPA = 0.0;
+                    System.out.println("GPA: " + GPA);
+                    break;
+                case SENTINEL :
+                    System.exit(1);
+                    break;
+                default: // Print "Invalid Grade" for an invalid grade
+                    System.out.println("Invalid grade.");
+                    break;
+            }
         }
     }
 }
